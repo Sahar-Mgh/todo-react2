120ca5d49bbbe71c71060aee0c87b8fb5ad56df4
Sahar <155814929+Sahar-Mgh@users.noreply.github.com>
2024-07-04 23:53:02 +0200

    project added

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import TodoItem from './TodoItem';
+import { useSelector } from 'react-redux';
 
-const TodoList = ({ todos, editTodo, deleteTodo, completeTodo }) => {
+const TodoList = () => {
+  const todos = useSelector((state) => state.todos);
+
   return (
     <ul className="todo-list">
       {todos.map(todo => (
         <TodoItem 
           key={todo.id} 
           todo={todo} 
-          editTodo={editTodo} 
-          deleteTodo={deleteTodo} 
-          completeTodo={completeTodo} 
         />
       ))}
     </ul>
---
+++ src/redux/reducers.js
@@ -0,0 +1,45 @@
+import { ADD_TODO, EDIT_TODO, DELETE_TODO, COMPLETE_TODO } from './actions';
+
+const initialState = {
+  todos: [],
+};
+
+const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          { id: Date.now(), text: action.payload, completed: false },
+        ],
+      };
+    case EDIT_TODO:
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload.id
+            ? { ...todo, text: action.payload.newText }
+            : todo
+        ),
+      };
+    case DELETE_TODO:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+    case COMPLETE_TODO:
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload
+            ? { ...todo, completed: !todo.completed }
+            : todo
+        ),
+      };
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
